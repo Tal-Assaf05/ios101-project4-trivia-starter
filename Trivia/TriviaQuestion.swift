615d746e37ac980a1756a6930e9fa1673138af3a
Tal-Assaf05 <tassa002@fiu.edu>
2025-03-26 22:56:58 -0400

    implemented code

--- Trivia/TriviaQuestion.swift
+++ Trivia/TriviaQuestion.swift
@@ -7,9 +7,20 @@
 
 import Foundation
 
-struct TriviaQuestion {
-  let category: String
-  let question: String
-  let correctAnswer: String
-  let incorrectAnswers: [String]
+struct TriviaQuestion: Codable {
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+    
+    enum CodingKeys: String, CodingKey {
+         case question
+         case correctAnswer = "correct_answer"
+         case incorrectAnswers = "incorrect_answers"
+         case category
+    }
+}
+
+struct MusicResponse: Codable {
+    let results: [TriviaQuestion]
 }
